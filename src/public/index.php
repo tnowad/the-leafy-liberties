5424840fbe2bdef542c4fcf90f3baf93f444091c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-31 22:31:42 +0700

    init

--- src/public/index.php
+++ src/public/index.php
@@ -1,14 +1,5 @@
 <?php
-
-use Sgu\Bookstore\MyClass;
-
 // show errors
 ini_set('display_errors', 1);
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-echo "ok";
-
-$myClass = new MyClass();
-
-echo "done";
+echo "init";
