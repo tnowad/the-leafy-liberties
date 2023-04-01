110200484640707652ceea160bdf3382a8fb212d
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-01 17:18:51 +0700

    add header footer

--- src/views/layouts/default/footer.php
+++ src/views/layouts/default/footer.php
@@ -1,2 +1,89 @@
-<?php
-echo 'footer';
+<footer
+  class="flex justify-center w-full flex-col items-center border-0 border-solid border-t-[1px] border-gray-400 mt-4 pt-5">
+  <div class="container sm:grid sm:m-auto lg:flex">
+    <div
+      class="left-container border-0 border-solid border-gray-400 pr-3 sm:border-r-0 sm:border-b-[1px] lg:border-r-[1px] lg:border-b-0 mobile:p-0">
+      <div class="sm:justify-center sm:items-center lg:flex lg:justify-start">
+        <div class="flex justify-center items-center"><img src="/static/media/bigLogo.99b1a4fb5773315580fe.png"
+            alt="logo"></div>
+        <div class="flex flex-col justify-center lg:flex mobile:hidden">
+          <p class="text-3xl border-0 border-solid border-b-[1px] border-gray-400 pb-1">Leaf Liberty</p>
+          <p class="">Book online store website</p>
+        </div>
+      </div>
+      <div class="description">
+        <p class="text-base mt-3 p-3 font-normal mobile:text-center lg:text-left">The LeafLiberty is a Book Store
+          E-commerce Website Designed by Tuan, Phat, Chinh, Bao</p>
+      </div>
+      <div class="w-full h-12 mt-6 pl-3">
+        <div class="flex justify-evenly"><svg aria-hidden="true" focusable="false" data-prefix="fab"
+            data-icon="facebook-f"
+            class="svg-inline--fa fa-facebook-f fa-2x hover:text-blue-500 transition-colors cursor-pointer" role="img"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" color="#999999">
+            <path fill="currentColor"
+              d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
+            </path>
+          </svg><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube"
+            class="svg-inline--fa fa-youtube fa-2x hover:text-red-500 transition-colors cursor-pointer" role="img"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" color="#999999">
+            <path fill="currentColor"
+              d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z">
+            </path>
+          </svg><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram"
+            class="svg-inline--fa fa-instagram fa-2x hover:text-pink-500 transition-colors cursor-pointer" role="img"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" color="#999999">
+            <path fill="currentColor"
+              d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
+            </path>
+          </svg><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter"
+            class="svg-inline--fa fa-twitter fa-2x hover:text-blue-400 transition-colors cursor-pointer" role="img"
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" color="#999999">
+            <path fill="currentColor"
+              d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z">
+            </path>
+          </svg></div>
+      </div>
+    </div>
+    <div class="right-container grid grid-cols-2 sm:grid-cols-4 justify-center text-center sm:w-full sm:mt-7 lg:w-3/4">
+      <div class="explore"><button class="text-xl font-medium mb-2">Explore</button>
+        <ul class="flex flex-col items-center w-full h-full text-gray-600">
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">About us</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Site map</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Bookmarks</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Pricing table</a></li>
+        </ul>
+      </div>
+      <div class="our-services"><button class="text-xl font-medium mb-2">Our services</button>
+        <ul class="flex flex-col items-center w-full h-full text-gray-600">
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Help center</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Product recalls</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Accessibility</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Store pickup</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Membership</a></li>
+        </ul>
+      </div>
+      <div class="categories"><button class="text-xl font-medium mb-2">Categories</button>
+        <ul class="flex flex-col items-center w-full h-full text-gray-600">
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Action</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Comedy</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Horror</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Kids</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">Drama</a></li>
+        </ul>
+      </div>
+      <div class="contact"><button class="text-xl font-medium mb-2">Get Contact</button>
+        <ul class="flex flex-col items-center w-full h-full text-gray-600">
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">SGU University</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">HCM City, VietNam</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">0123456789</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">0123456789</a></li>
+          <li class="h-12 flex items-center hover:text-green-800"><a href="/">sgu@edu.vn</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="w-full border-0 border-solid border-t-[1px] border-gray-400 text-center pt-4 mt-6 text-gray-500">
+    <p>The Leaf Liberty Bookstore E-commerce Website - © 2023 All Rights Reserved</p>
+  </div>
+</footer>
+<script src="https://cdn.tailwindcss.com"></script>
