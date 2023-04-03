7b2e3529201810c34717dec364175c09d5bc8c7e
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-03 11:01:44 +0700

    add asssets

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -6,12 +6,10 @@
   <title>
     <?php echo $title ?? 'Page Title'; ?>
   </title>
-  <link rel="stylesheet" href="./reset.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script src="https://cdn.tailwindcss.com"></script>
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./resources/css/all.css">
+  <script src="./resources/js/tailwindcss.js"></script>
+  <script src="./resources/js/flowbite.js"></script>
 </head>
 
 <body>
@@ -26,6 +24,5 @@
   require_once($footer ?? __DIR__ . '/default/footer.php');
   ?>
 </body>
-<script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
 
 </html>
