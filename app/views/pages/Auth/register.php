84de15f0eb4f5a6c49cdf2d060e8542d292bafe7
PhatJack <tienphatng.693@gmail.com>
2023-04-04 08:47:09 +0700

    fix color register and login

--- app/views/pages/Auth/register.php
+++ app/views/pages/Auth/register.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=test';
+$dsn = 'mysql:host=localhost;dbname=bookstore';
 $username = 'root';
 $password = '';
 
@@ -49,27 +49,26 @@ try {
 
 <section class="bg-white">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    <div
-      class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
       <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
           Create and account
         </h1>
-        <form class="space-y-4 md:space-y-6" action="#" method="post">
+        <form class="space-y-4 md:space-y-6" action="/views/pages/Auth/register.php" method="post">
           <div>
-            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="username" class="block mb-2 text-sm font-medium text-gray-900">
               Your email
             </label>
             <input type="text" name="username" id="username"
-              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#52938d] focus:border-[#52938d] block w-full p-2.5"
               placeholder="Enter username" required="" />
           </div>
           <div class='relative'>
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">
               Password
             </label>
             <input type="password" name="password" id="password" placeholder="••••••••"
-              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#52938d] focus:border-[#52938d] block w-full p-2.5"
               required="" />
             <i id="hide-icon-password" class="fa fa-eye-slash absolute top-[60%] right-4 cursor-pointer"
               aria-hidden="true"></i>
@@ -77,12 +76,12 @@ try {
               aria-hidden="true"></i>
           </div>
           <div class="relative">
-            <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900">
               Confirm password
             </label>
             <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••"
-              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required="" />
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#52938d] focus:border-[#52938d] block w-full p-2.5"
+              required />
             <i id="hide-icon-confirm-password" class="fa fa-eye-slash absolute top-[60%] right-4 cursor-pointer"
               aria-hidden="true"></i>
             <i id="show-icon-confirm-password" class="fa fa-eye absolute hidden top-[60%] right-4 cursor-pointer"
@@ -91,29 +90,28 @@ try {
           <div class="flex items-start">
             <div class="flex items-center h-5">
               <input id="terms" aria-describedby="terms" type="checkbox"
-                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-green-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-green-600 dark:ring-offset-gray-800"
-                required="" />
+                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-[#add1ce]" required />
             </div>
             <div class="ml-3 text-sm">
               <label for="terms" class="font-light text-gray-500 dark:text-gray-300">
                 I accept the
-                <Link class="font-medium text-green-600 hover:underline dark:text-green-500" href="#">
+                <a class="font-medium text-[#52938d] hover:underline" href="#">
                 Terms and Conditions
-                </Link>
+                </a>
               </label>
 
             </div>
 
           </div>
           <button type="submit"
-            class="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            class="w-full text-white bg-[#52938d] hover:bg-[#40736d] focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
             Create an account
           </button>
           <p class="text-sm font-light text-gray-500 dark:text-gray-400">
             Already have an account?
-            <Link to="/login" class="font-medium text-green-600 hover:underline dark:text-green-500">
+            <a href="/login" class="font-medium text-[#52938d] hover:underline">
             Login here
-            </Link>
+            </a>
           </p>
         </form>
       </div>
