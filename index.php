fa1fa8421192b2ebe822095c61cb51058ab1ad66
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-04 18:58:59 +0700

    fix database

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ if (ENVIRONMENT == 'development') {
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
+
+  function dd($data)
+  {
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+    die();
+  }
 }
 
 if (file_exists('./vendor/autoload.php')) {
