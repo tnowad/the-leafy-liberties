71230b4c5aba69ebb5c9d1d9f1fa278ab99f4c85
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-04 19:02:29 +0700

    add traits auth

--- core/traits/Auth.php
+++ core/traits/Auth.php
@@ -1,20 +1,35 @@
 <?php
 namespace Core;
 
+use App\Models\User;
+
 trait AuthTrait
 {
   public function authenticate($username, $password)
   {
-    // Implement authentication logic here
+    $user = User::where('username', $username)[0];
+
+    if (!$user) {
+      return false;
+    }
+
+    if (password_verify($password, $user->password)) {
+      $_SESSION['user'] = $user;
+      return true;
+    }
+
+    return false;
   }
 
   public function isLoggedIn()
   {
-    // Check if user is logged in and return true or false
+    return isset($_SESSION['user']);
   }
 
   public function requireLogin(Request $request, Response $response)
   {
-    // Redirect to login page if user is not logged in
+    if (!$this->isLoggedIn()) {
+      $response->redirect('/login');
+    }
   }
 }
