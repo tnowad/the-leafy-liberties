59eb89a3c1a0f496059c27fca95fc44386466126
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-04-08 18:25:32 +0700

    test

--- Test/test.php
+++ Test/test.php
@@ -5,15 +5,12 @@ require_once "../app/controllers/frontend/ProductController.php";
 
 $productController = new ProductController();
 
-// Test phương thức index()
 echo "Test phương thức index():<br>";
 $productController->index();
 
-// Test phương thức create()
 echo "<br>Test phương thức create():<br>";
 $productController->create();
 
-// Test phương thức store()
 echo "<br>Test phương thức store():<br>";
 $_POST['title'] = "Sách A";
 $_POST['author'] = "Tác giả A";
@@ -24,12 +21,10 @@ $_POST['description'] = "Mô tả sách A";
 $_POST['image_url'] = "https://example.com/image_a.jpg";
 $productController->store();
 
-// Test phương thức edit()
 echo "<br>Test phương thức edit():<br>";
 $id = 1;
 $productController->edit($id);
 
-// Test phương thức update()
 echo "<br>Test phương thức update():<br>";
 $_POST['title'] = "Sách B";
 $_POST['author'] = "Tác giả B";
@@ -40,6 +35,5 @@ $_POST['description'] = "Mô tả sách B";
 $_POST['image_url'] = "https://example.com/image_b.jpg";
 $productController->update($id);
 
-// Test phương thức delete()
 echo "<br>Test phương thức delete():<br>";
 $productController->delete($id);
