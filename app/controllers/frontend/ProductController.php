a3270a39e05346ff77e974ab22532368fc2e3567
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-04-09 22:10:01 +0700

    try code product

--- app/controllers/frontend/ProductController.php
+++ app/controllers/frontend/ProductController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controllers\Frontend;
 
 use Core\Controller;
@@ -6,30 +7,55 @@ use Core\Database;
 use Core\Request;
 use Core\Response;
 use Core\View;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
 
+  // public function index(Request $request, Response $response)
+  // {
+  //   dd($request->getQueries());
+  //   // Get all products by where
+  //   // pass in to variable
+  //   // render view
+  //   $response->setStatusCode(200);
+  //   $response->setBody(View::renderWithLayout(new View('pages/products'), [
+  //     'title' => 'Product',
+  //   ]));
+  // }
+
+
   public function index(Request $request, Response $response)
   {
-    dd($request->getQueries());
-    // Get all products by where
-    // pass in to variable
-    // render view
+    $products = Product::all();
+
     $response->setStatusCode(200);
-    $response->setBody(View::renderWithLayout(new View('pages/products'), [
-      'title' => 'Product',
+    $response->setBody(View::renderWithLayout(new View('pages/index'), [
+      'title' => 'Index',
+      'products' => $products,
     ]));
   }
 
+  // public function show(Request $request, Response $response)
+  // {
+  //   // Get product by id
+  //   // pass in to variable
+  //   // render view
+  //   $response->setStatusCode(200);
+  //   $response->setBody(View::renderWithLayout(new View('pages/product'), [
+  //     'title' => 'Product'
+  //   ]));
+  // }
+
   public function show(Request $request, Response $response)
   {
-    // Get product by id
-    // pass in to variable
-    // render view
+    $productId = $request->getParam('id');
+    $product = Product::find($productId);
+
     $response->setStatusCode(200);
-    $response->setBody(View::renderWithLayout(new View('pages/product'), [
-      'title' => 'Product'
+    $response->setBody(View::renderWithLayout(new View('pages/index'), [
+      'title' => 'Index',
+      'product' => $product,
     ]));
   }
 }
