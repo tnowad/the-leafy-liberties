100aceb4d9a2c9a608ded3b4a7dd46abbb724a0b
PhatJack <tienphatng.693@gmail.com>
2023-04-11 09:13:39 +0700

    add slider dashboard

--- app/views/pages/dashboard/slider.php
+++ app/views/pages/dashboard/slider.php
@@ -9,7 +9,7 @@
           <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
               <?php
-              $name = array("No", "Location", "Type", "Action");
+              $name = array("Title", "Image", "Location", "Action");
               for ($i = 1; $i <= count($name); $i++) { ?>
                 <th scope="col" class="px-6 py-3">
                   <?php echo $name[$i - 1] ?>
@@ -20,27 +20,34 @@
           </thead>
           <tbody>
             <?php
-            for ($i = 1; $i <= 3; $i++) { ?>
+            require_once 'app/views/pages/slides.php';
+            // print_r(basename($slides[0]['image']))
+
+            ?>
+            <?php foreach ($slides as $slide): ?>
               <tr class="bg-white border-b hover:bg-gray-200 transition-opacity even:bg-gray-100">
                 <td class="px-5 py-3 font-medium text-gray-900 whitespace-nowrap">
-                  <?php echo $i ?>
+                  <?php echo $slide['title'] ?>
                 </td>
-                <td class="px-5 py-3">Home Slider</td>
+                <td class="px-5 py-3 w-64">
+                  <img src="../<?php echo $slide['image'] ?>" alt="" />
+                </td>
                 <td class="px-5 py-3">Image</td>
                 <td class="px-5 py-3 w-44">
-                  <label class="relative inline-flex items-center cursor-pointer shadow-lg rounded-full border-solid border-blue-300">
+                  <label
+                    class="toggle-btn relative inline-flex items-center cursor-pointer shadow-lg rounded-full border-solid border-blue-300">
                     <input type="checkbox" value="" class="sr-only peer">
                     <div
                       class="w-20 h-8 bg-gray-700 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-[50px] peer-checked:after:border-white after:content-[''] after:absolute after:left-[0px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-8 after:w-8 after:transition-all peer-checked:bg-blue-600">
                     </div>
-                    <span class="ml-3 text-sm font-medium text-gray-700 absolute -left-2 peer-checked:text-blue-600">Off</span>
-                    <span class="ml-3 text-sm font-medium text-gray-700 absolute left-11 peer-checked:text-gray-900">On</span>
-
+                    <span
+                      class="ml-3 text-sm font-medium text-gray-700 absolute -left-2 peer-checked:text-blue-600">Off</span>
+                    <span
+                      class="ml-3 text-sm font-medium text-gray-700 absolute left-11 peer-checked:text-gray-900">On</span>
                   </label>
                 </td>
               </tr>
-            <?php }
-            ?>
+            <?php endforeach ?>
           </tbody>
         </table>
       </div>
@@ -48,4 +55,14 @@
   </div>
 </div>
 <script>
+  let input_check = document.querySelector('input[type="checkbox"]');
+  let btn_toggle = document.querySelector('.toggle-btn');
+  <?php foreach ($slides as $slide): ?>
+    if(input_chec.checked == false){
+      <?php $slide['image'] = '' ?>
+    }
+    else{
+      continue;
+    }
+  <?php endforeach ?>
 </script>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -4,33 +4,7 @@
       <div class="relative h-56 overflow-hidden rounded-lg sm:h-64 xl:h-80 2xl:h-96 -z-10">
 
         <?php
-        $slides = [
-          [
-            'title' => 'Slide 1',
-            'image' => 'resources/images/banner/banner-books-1.png',
-            'alt' => 'Slide 1'
-          ],
-          [
-            'title' => 'Slide 2',
-            'image' => 'resources/images/banner/banner-books-2.jpg',
-            'alt' => 'Slide 2'
-          ],
-          [
-            'title' => 'Slide 3',
-            'image' => 'resources/images/banner/banner-books-3.png',
-            'alt' => 'Slide 3'
-          ],
-          [
-            'title' => 'Slide 4',
-            'image' => 'resources/images/banner/banner-books-4.png',
-            'alt' => 'Slide 4'
-          ],
-          [
-            'title' => 'Slide 5',
-            'image' => 'resources/images/banner/banner-books-5.png',
-            'alt' => 'Slide 5'
-          ]
-        ];
+        require_once 'app/views/pages/slides.php';
         ?>
 
         <?php foreach ($slides as $slide) : ?>
