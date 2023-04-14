2027a5bd5e238b2d51d7341aeab1255e861dbe53
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-14 07:30:16 +0700

    update cart

--- app/controllers/frontend/CartController.php
+++ app/controllers/frontend/CartController.php
@@ -5,6 +5,7 @@ namespace App\Controllers\Frontend;
 use App\Models\Author;
 use App\Models\Cart;
 use App\Models\Product;
+use Core\Application;
 use Core\Controller;
 use Core\Database;
 use Core\Request;
@@ -17,20 +18,22 @@ class CartController extends Controller
 
   public function index(Request $request, Response $response)
   {
-    try {
-      $cart = Cart::all();
-      $products = Product::all();
-      $authors = Author::all();
-    } catch (\Exception $e) {
-      dd($e->getMessage());
+    $auth = Application::getInstance()->getAuthentication();
+    if (!$auth->isAuthenticated()) {
+      return $response->redirect('/login');
+    }
+    $cartItems = Cart::where(['user_id' => $auth->getUser()->id]);
+    $products = [];
+
+    // Get all products in cart
+    foreach ($cartItems as $cartItem) {
+      $product = Product::find($cartItem->product_id);
+      $product->quantity = $cartItem->quantity;
+      $products[] = $product;
     }
 
-    $response->setStatusCode(200);
-    $response->setBody(View::renderWithLayout(new View('pages/cart'), [
-      'title' => 'cart',
-      'cart' => $cart,
+    return $response->setBody(View::render(new View('pages/cart/index'), [
       'products' => $products,
-      'authors' => $authors,
     ]));
   }
 }
