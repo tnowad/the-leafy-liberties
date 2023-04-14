8dda402277830b025a555989072c8c4995dd5b65
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-14 09:06:28 +0700

    add order model

---
+++ app/models/Order.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+
+use App\Models\OrderProduct;
+use App\Models\User;
+use Core\Model;
+
+class Order extends Model
+{
+  protected $table = 'orders';
+  protected $fillable = ['user_id', 'status', 'total', 'created_at', 'updated_at', 'deleted_at'];
+
+  public function user()
+  {
+    return User::find($this->user_id);
+  }
+
+  public function orderItems()
+  {
+    return OrderProduct::where(['order_id' => $this->id]);
+  }
+}
---
+++ app/models/OrderProduct.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Models;
+
+use App\Models\Product;
+use Core\Model;
+
+class OrderProduct extends Model
+{
+  protected $table = 'order_products';
+  protected $fillable = ['order_id', 'product_id', 'quantity', 'price', 'created_at', 'updated_at', 'deleted_at'];
+
+  public function order()
+  {
+    return Order::find($this->order_id);
+  }
+
+  public function product()
+  {
+    return Product::find($this->product_id);
+  }
+}
