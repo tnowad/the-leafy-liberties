f2f6f36542712b6300414ef5099c825fe6522f5d
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2023-04-14 16:51:18 +0700

    code of Bao in order view

--- app/views/pages/profile/orders.php
+++ app/views/pages/profile/orders.php
@@ -1,53 +1,57 @@
 <?php $user = $params['user']; ?>
 <div class="flex justify-center w-full bg-white">
-  <div class="container">
-    <div class="box-border flex flex-col w-full mt-10 border border-b-2 border-gray-300 md:flex-row">
-      <?php include 'menu.php'; ?>
-      <!-- content -->
-      <div class="w-full p-2 md:w-3/4">
-        <form class="flex flex-col" action="<?php echo BASE_URI . '/profile' ?>" method="post">
-          <label>Name</label>
-          <input name="name" value="<?php echo $user->name ?>" type="text" required class="w-full p-5 duration-300 border border-gray-300 border-solid rounded-md h-9 hover:shadow-sm" />
-          <label>Email</label>
-          <input name="email" value=" <?php echo $user->email ?>" type="email" required class="w-full p-5 duration-300 border border-gray-300 border-solid rounded-md h-9 hover:shadow-sm" />
-          <label>Phone number</label>
-          <input name="phone" value="<?php echo $user->phone ?>" type="tel" class="w-full p-5 duration-300 border border-gray-300 border-solid rounded-md h-9 hover:shadow-sm" pattern="^(\+84|0)(1\d{9}|3\d{8}|5\d{8}|7\d{8}|8\d{8}|9\d{8})$" required></input>
-          <label>Gender</label>
-          <div class="relative inline-block">
-            <select name="gender" id="gender" class="block w-full px-4 py-2 pr-8 leading-tight duration-300 bg-white border border-gray-400 rounded shadow appearance-none hover:shadow-sm focus:outline-none focus:shadow-outline">
-              <option value="male" <?php if ($user->gender == "1") {
-                                      echo "selected";
-                                    } ?>>Male</option>
-              <option value="female" <?php if ($user->gender == "2") {
-                                        echo "selected";
-                                      } ?>>Female</option>
-              <option value="other" <?php if ($user->gender == "0") {
-                                      echo "selected";
-                                    } ?>>Other</option>
-            </select>
-          </div>
-
-          <label>Birthday</label>
-          <div class="flex justify-between w-full h-8 md:h-10 md:w-2/4">
-            <input name="birthday" type="date" value="<?php echo $user->birthday ?>" />
-          </div>
-          <label>Password change</label>
-          <div class="flex flex-col p-2 border border-gray-300 border-solid rounded">
-            <label for="password">
-              Current password (leave blank to leave unchanged)
-            </label>
-            <input name="password" type="text" class="w-full p-5 border border-gray-300 border-solid rounded-md h-9" />
-            <label for="new-password">Confirm new password</label>
-            <input name="new-password" type="text" class="w-full p-5 border border-gray-300 border-solid rounded-md h-9" />
-          </div>
-          <input type="submit" value="Save changes" class="w-3/6 p-2 mt-5 text-white   bg-primary-800 hover:bg-teal-700 border cursor-pointer sm:w-1/6 rounded-2xl transition-all" />
-        </form>
-      </div>
+    <div class="container">
+        <div class="box-border flex flex-col w-full mt-10 border border-b-2 border-gray-300 md:flex-row">
+            <?php include 'menu.php'; ?>
+            <!-- content -->
+            <div class="table-statistics my-8 shadow-lg cursor-pointer rounded-2xl overflow-hidden bg-white">
+                <div class="relative">
+                    <table class="w-full text-sm text-center text-gray-500 rounded-2xl overflow-y-scroll h-64">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <?php
+                $name = array("STT", "booking date", "Status", "Total Price", "Action");
+                for ($i = 1; $i <= 5; $i++) { ?>
+                                <th scope="col" class="px-16 py-3">
+                                    <?php echo $name[$i - 1] ?>
+                                </th>
+                                <?php }
+                ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+              for ($i = 1; $i <= 6; $i++) { ?>
+                            <tr class="bg-white border-b hover:bg-gray-200 transition-opacity even:bg-gray-100">
+                                <!-- <td class="px-5 py-3">#1</td> -->
+                                <td class="px-5 py-3"></td>
+                                <td class="px-5 py-3">30/02/2023</td>
+                                <td class="px-5 py-3">Accept</td>
+                                <td class="px-5 py-3">100000</td>
+                                <td class="px-5 py-4 w-44">
+                                    <div class="button flex justify-center items-center gap-4">
+                                        <button
+                                            class="edit-button py-2 px-3 bg-[#8cbfba] text-white rounded-xl hover:text-blue-500 transition-all">
+                                            <i class="fa-solid fa-pen-to-square"></i>
+                                        </button>
+                                        <button
+                                            class="delete-button py-2 px-3 bg-[#8cbfba] text-white rounded-xl hover:text-red-600 transition-all">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php }
+              ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 <script>
-  async function getUser() {
+async function getUser() {
     let d1 = await fetch("http://localhost/the-leafy-liberties/profile")
     let d2 = await d1.json()
     console.log(d2)
@@ -55,7 +59,7 @@
     // document.getElementById("gender").value = d2.gender;
     // document.getElementById("birthday").value = d2.birthday;
     // document.getElementById("password").value = d2.password;
-  }
-  console.log(1)
-  getUser()
+}
+console.log(1)
+getUser()
 </script>
