1606a432c81cdb369c48d2c76796fa59a2bf0888
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-15 19:15:36 +0700

    fix model

--- app/models/Role.php
+++ app/models/Role.php
@@ -15,26 +15,25 @@ class Role extends Model
 
   public function permissions()
   {
-    $rolePermissions = RolePermission::findAll(['role_id' => $this->id]);
-    $permissions = [];
-    foreach ($rolePermissions as $rolePermission) {
-      $permissions[] = Permission::find($rolePermission->permission_id);
-    }
-    return $permissions;
+    return RolePermission::findAll([
+      'role_id' => $this->id
+    ]);
   }
 
   public function addPermission(Permission $permission)
   {
-    $rolePermission = RolePermission::findAll([
-      'role_id' => $this->id,
-      'permission_id' => $permission->id
-    ]);
-    if ($rolePermission) {
-      return;
-    }
     $rolePermission = new RolePermission();
     $rolePermission->role_id = $this->id;
     $rolePermission->permission_id = $permission->id;
     $rolePermission->save();
+  }
+
+  public function removePermission(Permission $permission)
+  {
+    $rolePermission = RolePermission::findOne([
+      'role_id' => $this->id,
+      'permission_id' => $permission->id
+    ]);
+    $rolePermission->delete();
   }
 }
