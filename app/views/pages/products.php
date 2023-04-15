14e6c2f4c2f896dbe0db91ff013f53c8ba210a8a
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-04-15 10:28:14 +0700

    filter price products

--- app/views/pages/products.php
+++ app/views/pages/products.php
@@ -4,38 +4,39 @@
       <div class="fixed">
         <h1 class="mb-2 text-2xl font-bold">Filter</h1>
         <div class="grid justify-around grid-cols-3 lg:block">
-          <form class="w-full" method="POST" action="">
+          <form class="w-full" method="" action="">
             <h1 class="mt-2 mb-2 text-xl font-bold">Deals</h1>
             <div>
-              <input type="radio" name="deals" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
-              <label htmlFor="" class="ml-2">
+              <input type="radio" name="deals" id="best-value" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
+              <label for="best-value" class="ml-2">
                 Best value from shop
               </label>
               <br>
-              <input type="radio" name="deals" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
-              <label htmlFor="" class="ml-2">
+              <input type="radio" name="deals" id="member-only" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
+              <label for="member-only" class="ml-2">
                 Member only
               </label>
             </div>
             <h1 class="mt-2 mb-2 text-xl font-bold">Your budget</h1>
             <div>
-              <input type="radio" name="yourBudget" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
-              <label htmlFor="" class="ml-2">
+              <input type="radio" name="yourBudget" id="less-than-10" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
+              <label for="less-than-10" class="ml-2">
                 Less than $10
               </label>
               <br>
-              <input type="radio" name="yourBudget" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
-              <label htmlFor="" class="ml-2">
+              <input type="radio" name="yourBudget" id="11-to-30" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
+              <label for="11-to-30" class="ml-2">
                 $11-$30
               </label>
               <br>
-              <input type="radio" name="yourBudget" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
-              <label htmlFor="" class="ml-2">
+              <input type="radio" name="yourBudget" id="greater-than-30" class="w-4 h-4 text-green-600 bg-gray-300 rounded focus:ring-green-500 " />
+              <label for="greater-than-30" class="ml-2">
                 Greater than $30
               </label>
             </div>
-            <input type="submit" value="Find" class="py-2 px-5 bg-[#315854] font-semibold text-white rounded-lg my-5 hover:bg-[#6cada6] transition-all cursor-pointer" />
+            <input type="submit" value="Find" onclick="filterProducts(event)" class="py-2 px-5 bg-[#315854] font-semibold text-white rounded-lg my-5 hover:bg-[#6cada6] transition-all cursor-pointer" />
           </form>
+
         </div>
       </div>
     </div>
@@ -59,11 +60,6 @@
                   <?php echo $product->name ?>
                 </a>
               </div>
-              <!-- <div class=" product-rate">
-                <?php for ($i = 1; $i <= $product->star; $i++) : ?>
-                  <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-                <?php endfor; ?>
-              </div> -->
               <div class="text-sm text-gray-500 product-author">
                 <?php echo $author->name ?>
               </div>
@@ -101,10 +97,82 @@
 
 
 <script>
-  const getUsers = async () => {
-    let d1 = await fetch("http://localhost/the-leafy-liberties/data/getProducts")
-    let d2 = await d1.json()
-    console.log(d2)
+  function renderProducts(products) {
+    const productContainer = document.getElementById('product-list');
+    productContainer.innerHTML = '';
+
+    products.forEach(product => {
+      const productElement = document.createElement('div');
+      productElement.classList.add('product');
+
+      productContainer.innerHTML += `
+          <div class="flex flex-col items-center justify-center w-full px-[22px] box-border pt-5 product-info group border-solid border hover:border-gray-500 transition-all hover:shadow-xl">
+            <div class="object-cover h-full p-2 w-60">
+              <img src="resources/images/products/${product.image}" alt="" class="object-cover w-full h-full" />
+            </div>
+            <div class="flex flex-col items-start justify-center w-full p-1 text-lg font-medium transition-all bg-white product-body group-hover:-translate-y-16">
+              <div class="product-name">
+                <a href="/">
+               ${product.name}
+                </a>
+              </div>
+              <div class="text-sm text-gray-500 product-author">
+                Gia Bảo
+              </div>
+              <div class="p-0 font-semibold product-price text-primary-900">
+              ${product.price}
+              </div>
+              <div class="flex items-center justify-between w-full transition-all translate-y-0 opacity-0 heart-option group-hover:opacity-100">
+                <p class="font-semibold select-option-text">Add to wishlist</p>
+                <i class="p-2 transition-all rounded-full cursor-pointer fa-regular fa-heart hover:bg-red-400 hover:text-white"></i>
+              </div>
+            </div>
+          </div>
+          `;
+    });
   }
-  getUsers()
+
+  function filterByPrice(products, priceRange) {
+    let filteredProducts = [];
+    // console.log(products)
+
+
+    for (let i = 0; i < products.length; i++) {
+      let product = products[i];
+      if (priceRange === 'lessThan10' && product.price < 10) {
+        filteredProducts.push(product);
+      } else if (priceRange === 'between11And30' && product.price >= 11 && product.price <= 30) {
+        filteredProducts.push(product);
+      } else if (priceRange === 'greaterThan30' && product.price > 30) {
+        filteredProducts.push(product);
+      }
+    }
+
+    return filteredProducts;
+  }
+
+
+
+  async function filterProducts(event) {
+    event.preventDefault();
+
+    let products = await fetch("http://localhost/the-leafy-liberties/data/getProducts").then((response) => response.json())
+
+    const bestValue = document.getElementById('best-value').checked;
+    const memberOnly = document.getElementById('member-only').checked;
+    const lessThan10 = document.getElementById('less-than-10').checked;
+    const between11And30 = document.getElementById('11-to-30').checked;
+    const greaterThan30 = document.getElementById('greater-than-30').checked;
+
+    let filteredProducts = [];
+    if (lessThan10) {
+      filteredProducts = filterByPrice(products, "lessThan10");
+    } else if (between11And30) {
+      filteredProducts = filterByPrice(products, "between11And30");
+    } else if (greaterThan30) {
+      filteredProducts = filterByPrice(products, "greaterThan30");
+    }
+
+    renderProducts(filteredProducts);
+  }
 </script>
