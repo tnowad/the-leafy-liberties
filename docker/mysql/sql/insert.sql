60603069928f469aab17d9b34d678043785d1eae
HOANGGIABAO212 <hoanggiabao2122003@gmail.com>
2023-04-17 09:20:10 +0700

    fix pagination

--- docker/mysql/sql/insert.sql
+++ docker/mysql/sql/insert.sql
@@ -515,6 +515,28 @@ VALUES (
         NULL
     );
 
+INSERT INTO products_categories (product_id, category_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 1),
+(11, 2),
+(12, 3),
+(13, 4),
+(14, 5),
+(15, 6),
+(16, 7),
+(17, 8),
+(18, 9),
+(19, 1),
+(20, 2);
+
 SET FOREIGN_KEY_CHECKS = 1;
 
 COMMIT;
