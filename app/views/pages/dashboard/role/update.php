f2bf6ff01999dd100d348129aa13d06aef37388c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-20 14:34:27 +0700

    fix

--- app/views/pages/dashboard/role/update.php
+++ app/views/pages/dashboard/role/update.php
@@ -27,11 +27,17 @@ if ($allPermissions['dashboard.access']['status'] == 0) {
     }
   }
 }
-$content = ['Dashboard', 'Users', 'Product', 'Author', 'Publisher', 'Slider', 'Role', 'Coupon', 'Cart', 'Category', 'Permission', 'Setting','Order','Wishlist'];
-$allPermissions = array_values($allPermissions);
+
+$groupPermissions = [];
+
+foreach ($allPermissions as $key => $permission) {
+  $group = explode('.', $key)[0];
+  $groupPermissions[$group][] = $permission;
+}
+
 ?>
 
-<form class="mt-6 min-h-screen">
+<form class="min-h-screen mt-6">
   <div class="mb-4">
     <label class="block font-bold text-gray-700" for="name">
       Name
@@ -49,57 +55,32 @@ $allPermissions = array_values($allPermissions);
     Update
   </button>
 
-  <table class="w-full h-64 text-sm text-center text-gray-500 rounded-2xl mt-5 cursor-pointer" width="100%">
-    <thead class="text-md text-gray-700 uppercase bg-gray-50 p-2">
+  <table class="w-full h-64 mt-5 text-sm text-center text-gray-500 rounded-2xl" width="100%">
+    <thead class="p-2 text-gray-700 uppercase text-md bg-gray-50">
       <tr>
-        <th class="py-3 px-4" width="50%">Permissions</th>
-        <th class="py-3 px-4">Access</th>
-        <th class="py-3 px-4">Show</th>
-        <th class="py-3 px-4">Delete</th>
-        <th class="py-3 px-4">Create</th>
-        <th class="py-3 px-4">Update</th>
+        <th class="px-4 py-3" width="50%">Permissions</th>
+        <th class="px-4 py-3">Access</th>
+        <th class="px-4 py-3">Show</th>
+        <th class="px-4 py-3">Delete</th>
+        <th class="px-4 py-3">Create</th>
+        <th class="px-4 py-3">Update</th>
       </tr>
     </thead>
     <tbody>
 
-      <?php foreach ($content as $item): ?>
+      <?php foreach ($groupPermissions as $groupName => $groupPermission): ?>
         <tr class="text-center transition-opacity bg-white border-b hover:bg-gray-200 even:bg-gray-100 !text-md">
           <td class="px-5 py-3">
             <?php echo $item ?>
           </td>
-
-          <!-- <?php foreach ($allPermissions as $permission): ?>
+          <?php foreach ($groupPermission as $permission): ?>
             <td>
               <input
                 class="mr-3 text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 type="checkbox" name="permissions[]" id="<?php echo $permission['name'] ?>"
                 value="<?php echo $permission['id'] ?>" <?php echo $permission['status'] == 1 ? 'checked' : '' ?>>
             </td>
-            <td>
-              <input
-                class="mr-3 text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                type="checkbox" name="permissions[]" id="<?php echo $permission['name'] ?>"
-                value="<?php echo $permission['id'] ?>" <?php echo $permission['status'] == 1 ? 'checked' : '' ?>>
-            </td>
-            <td>
-              <input
-                class="mr-3 text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                type="checkbox" name="permissions[]" id="<?php echo $permission['name'] ?>"
-                value="<?php echo $permission['id'] ?>" <?php echo $permission['status'] == 1 ? 'checked' : '' ?>>
-            </td>
-            <td>
-              <input
-                class="mr-3 text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                type="checkbox" name="permissions[]" id="<?php echo $permission['name'] ?>"
-                value="<?php echo $permission['id'] ?>" <?php echo $permission['status'] == 1 ? 'checked' : '' ?>>
-            </td>
-            <td>
-              <input
-                class="mr-3 text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                type="checkbox" name="permissions[]" id="<?php echo $permission['name'] ?>"
-                value="<?php echo $permission['id'] ?>" <?php echo $permission['status'] == 1 ? 'checked' : '' ?>>
-            </td>
-          <?php endforeach ?> -->
+          <?php endforeach ?>
         </tr>
       <?php endforeach ?>
     </tbody>
