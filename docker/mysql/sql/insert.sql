29843e464f274fe706f4ba1e83c1b7593ea630d5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-21 20:48:21 +0700

    remvoe comma

--- docker/mysql/sql/insert.sql
+++ docker/mysql/sql/insert.sql
@@ -302,7 +302,7 @@ INSERT INTO `shipping_methods` (`id`, `name`, `price`, `description`, `status`, 
 (1, 'Standard Shipping', 5.99, 'Delivery within 7-10 business days', 1, NULL),
 (2, 'Express Shipping', 14.99, 'Delivery within 2-3 business days', 1, NULL),
 (3, 'Next Day Shipping', 29.99, 'Guaranteed delivery next business day', 1, NULL),
-(4, 'International Shipping', 24.99, 'Delivery within 10-14 business days to international destinations', 1, NULL),
+(4, 'International Shipping', 24.99, 'Delivery within 10-14 business days to international destinations', 1, NULL);
 
 INSERT INTO `slides` (`id`, `image`, `name`, `status`, `deleted_at`) VALUES
 (1, '/resources/images/slides/slide-books-1.jpg', 'book festival', 1, NULL),
