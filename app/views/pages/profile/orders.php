bf3a141c3da0ac33258be73bb4da199c3e8bc942
HOANGGIABAO212 <hoanggiabao2122003@gmail.com>
2023-04-21 21:10:40 +0700

    fix order profile

--- app/views/pages/profile/orders.php
+++ app/views/pages/profile/orders.php
@@ -76,11 +76,8 @@ $orders = $params['orders'];
                     <?php endif; ?>
                   </td>
                   <td class="px-5 py-3">
-                    <!-- icon view -->
                     <a href="<?php echo BASE_URI . '/profile/orders?id=' . $order->id ?>" class="edit-button py-2 px-3 bg-[#8cbfba] text-white rounded-xl hover:text-blue-500 transition-all">
-                      <i class="fa-solid fa-pen-to-square"></i>
-                    </a>
-                    <a href="">
+                      <!-- <i class="fa-solid fa-pen-to-square"></i> -->
                       <i class="text-blue-500 fas fa-eye"></i>
                     </a>
                   </td>
